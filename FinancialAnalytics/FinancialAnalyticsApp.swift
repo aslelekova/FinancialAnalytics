9cf78c5dc149d27c7fcb5d5b779123011dd3f35d
Anastasia Lelekova <alelekova36@gmail.com>
2024-02-06 20:39:48 +0300

    Initial Commit

---
+++ FinancialAnalytics/FinancialAnalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialAnalyticsApp.swift
+//  FinancialAnalytics
+//
+//  Created by Анастасия Лелекова on 06.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct FinancialAnalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
